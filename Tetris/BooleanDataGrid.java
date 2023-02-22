daa9e36698c8d748355b5076cd15c1be81b556b6
Printerestingly <jirattakiet1234@gmail.com>
2023-02-22 17:24:04 +0700

    KickTable + BooleanDataGrid

---
+++ Tetris/BooleanDataGrid.java
@@ -0,0 +1,26 @@
+package Tetris;
+
+public class BooleanDataGrid {
+    public boolean[][] field;
+    
+    public BooleanDataGrid(int w, int h){    
+        this.field = new boolean[w][h];
+    }
+
+    public boolean getAtPos(int x, int y){
+        return field[x][y];
+    }
+
+    public int getWidth(){
+        return field.length;
+    }
+
+    public int getHeight(){
+        return field[0].length;
+    } 
+
+    public void setAtPos(int x, int y, boolean input){
+        field[x][y] = input;
+    }
+    
+}
--- Tetris/KickTable.java
+++ Tetris/KickTable.java
@@ -6,18 +6,18 @@ public class KickTable {
     private Map<Direction, Map<Direction, XY>> table;
   
     public XY getKick(Direction beginDir, Direction destDir){
-        
+        return table.get(beginDir).get(destDir);
     }
     private KickTable(KickTableBuilder ktb) {
       this.table = ktb.table;
     }
     
     public static class KickTableBuilder {
-      private Map<Direction, Map<Direction, XY>> table
+      private Map<Direction, Map<Direction, XY>> table;
       public KickTableBuilder() {
         ...
       }
-      public void setKick(beginDir, destDir, kick) {
+      public void setKick(Direction beginDir,Direction destDir,XY kick) {
         ...
       }
       public KickTable build() {
