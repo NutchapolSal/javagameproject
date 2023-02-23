b789bd9ee7bc629f7646de31c4dc420e18182803
NutchapolSal <Nutchapol.sal@gmail.com>
2023-02-23 11:42:02 +0700

    minoorigin changed getters to public final attributes

--- Tetris/MinoOrigin.java
+++ Tetris/MinoOrigin.java
@@ -1,31 +1,15 @@
 package Tetris;
 
 public class MinoOrigin {
-    protected int x;
-    protected int y;
-    protected boolean xOffset;
-    protected boolean yOffset;
+    public final int x;
+    public final int y;
+    public final boolean xOffset;
+    public final boolean yOffset;
 
-    public MinoOrigin (int x,int  y ,boolean xOffset,boolean yOffset){
+    public MinoOrigin(int x, int y, boolean xOffset, boolean yOffset) {
         this.x = x;
         this.y = y;
         this.xOffset = xOffset;
         this.yOffset = yOffset;
-    }
-
-    public int getX(){
-        return x;
-    }
-
-    public int getY(){
-        return y;
-    }
-
-    public boolean getXOffset(){
-        return xOffset;
-    }
-
-    public boolean getYOffset(){
-        return yOffset;
     }
 }
