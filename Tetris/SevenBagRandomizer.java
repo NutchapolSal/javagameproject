8eb76424b25a9dc8a27c7b7026e5e2dfafe05a60
SaveKit <kittipit582@gmail.com>
2023-02-27 16:09:57 +0700

    write MinoRandomizer and SevenBagRandomizer

---
+++ Tetris/SevenBagRandomizer.java
@@ -0,0 +1,57 @@
+package Tetris;
+
+import java.io.Console;
+import java.util.Random;
+
+public class SevenBagRandomizer extends MinoRandomizer {
+
+    private final Mino[] bag = new Mino[7];
+    private int bagIndex;
+    private final Random random;
+
+    public SevenBagRandomizer(long seed) {
+        super(seed);
+        random = new Random(seed);
+        fillBag();
+        shuffleMinoInBag();
+    }
+
+    private void fillBag() {
+        bag[0] = Tetromino.I();
+        bag[1] = Tetromino.L();
+        bag[2] = Tetromino.J();
+        bag[3] = Tetromino.O();
+        bag[4] = Tetromino.S();
+        bag[5] = Tetromino.T();
+        bag[6] = Tetromino.Z();
+    }
+
+    private void shuffleMinoInBag() {
+        for (int i = 0; i < bag.length; i++) {
+            int randomIndex = random.nextInt(bag.length - i) + i;
+            Mino temp = bag[i];
+            bag[i] = bag[randomIndex];
+            bag[randomIndex] = temp;
+        }
+        bagIndex = 0;
+    }
+
+    @Override
+    public Mino next() {
+        if (bagIndex == bag.length) {
+            shuffleMinoInBag();
+        }
+        return bag[bagIndex++];
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        var sbr = new SevenBagRandomizer(987);
+        while (true) {
+            for (int i = 0; i < 7; i++) {
+                System.out.print(sbr.next().getColor() + " ");
+            }
+            System.out.println();
+            // Thread.sleep(500);
+        }
+    }
+}
