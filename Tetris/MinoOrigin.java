6ec6f145a5aca585b6af5662f1736405093c8e94
NutchapolSal <Nutchapol.sal@gmail.com>
2023-02-27 10:27:59 +0700

    javadoc for minoorigin offsets

--- Tetris/MinoOrigin.java
+++ Tetris/MinoOrigin.java
@@ -3,7 +3,13 @@ package Tetris;
 public class MinoOrigin {
     public final int x;
     public final int y;
+    /**
+     * true means origin should be moved by -0.5 for x axis
+     */
     public final boolean xOffset;
+    /**
+     * true means origin should be moved by -0.5 for y axis
+     */
     public final boolean yOffset;
 
     public MinoOrigin(int x, int y, boolean xOffset, boolean yOffset) {
