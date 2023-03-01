63c47fc47ecec35988e131abaa077349d3a9f287
NutchapolSal <Nutchapol.sal@gmail.com>
2023-03-02 00:36:43 +0700

    mino name these constructors are getting stupid

--- Tetris/Mino.java
+++ Tetris/Mino.java
@@ -7,6 +7,7 @@ public class Mino implements ShapeGrid, KickTable {
     protected MinoOrigin origin;
     protected KickTable kickTable = (new KickTableBuilder()).build();
     protected MinoColor color = MinoColor.White;
+    protected String name = "";
 
     public Mino(ShapeGrid shape, MinoOrigin origin) {
         this.shape = shape;
@@ -32,6 +33,14 @@ public class Mino implements ShapeGrid, KickTable {
         this.color = color;
     }
 
+    public Mino(ShapeGrid shape, MinoOrigin origin, KickTable kickTable, MinoColor color, String name) {
+        this.shape = shape;
+        this.origin = origin;
+        this.kickTable = kickTable;
+        this.color = color;
+        this.name = name;
+    }
+
     public boolean getAtPos(int x, int y) {
         return this.shape.getAtPos(x, y);
     }
@@ -55,5 +64,12 @@ public class Mino implements ShapeGrid, KickTable {
 
     public MinoOrigin getOrigin() {
         return origin;
+    }
+
+    /**
+     * @return mino's name (empty string if not named)
+     */
+    public String getName() {
+        return name;
     }
 }
