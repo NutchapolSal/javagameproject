5fd438ab64dae7202e4aff1012abdbe5f52894b9
NutchapolSal <Nutchapol.sal@gmail.com>
2023-03-03 14:51:54 +0700

    more guiData fields

--- Tetris/GuiData.java
+++ Tetris/GuiData.java
@@ -7,23 +7,42 @@ public class GuiData {
     public final Mino[] nextQueue;
     public final Mino hold;
     public final boolean lockHold;
-    public final ObjectDataGrid<MinoColor> renderBlocks;
+    public final PlayerRenderData playerRenderData;
     public final double windowNudgeX;
     public final double windowNudgeY;
-    public final PlayerRenderData playerRenderData;
+    public final ObjectDataGrid<MinoColor> renderBlocks;
+    public final int calloutLines;
+    public final String spinText;
+    public final int comboCount;
+    public final int b2bCount;
 
-    public GuiData(long timeMillis, int linesCleared, int level, Mino[] nextQueue, Mino hold, boolean lockHold,
-            ObjectDataGrid<MinoColor> renderBlocks, double windowNudgeX, double windowNudgeY,
-            PlayerRenderData playerRenderData) {
+    public GuiData(long timeMillis,
+            int linesCleared,
+            int level,
+            Mino[] nextQueue,
+            Mino hold,
+            boolean lockHold,
+            PlayerRenderData playerRenderData,
+            double windowNudgeX,
+            double windowNudgeY,
+            ObjectDataGrid<MinoColor> renderBlocks,
+            int calloutLines,
+            String spinText,
+            int comboCount,
+            int b2bCount) {
         this.timeMillis = timeMillis;
         this.linesCleared = linesCleared;
         this.level = level;
         this.nextQueue = nextQueue;
         this.hold = hold;
         this.lockHold = lockHold;
-        this.renderBlocks = renderBlocks;
+        this.playerRenderData = playerRenderData;
         this.windowNudgeX = windowNudgeX;
         this.windowNudgeY = windowNudgeY;
-        this.playerRenderData = playerRenderData;
+        this.renderBlocks = renderBlocks;
+        this.calloutLines = calloutLines;
+        this.spinText = spinText;
+        this.comboCount = comboCount;
+        this.b2bCount = b2bCount;
     }
 }
