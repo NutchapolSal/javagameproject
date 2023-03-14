43ad2406a12bdb0b1c775636a75b902f160ebfb1
NutchapolSal <Nutchapol.sal@gmail.com>
2023-03-14 11:43:31 +0700

    playfield panel callout

--- Tetris/PlayfieldPanel.java
+++ Tetris/PlayfieldPanel.java
@@ -2,12 +2,17 @@ package Tetris;
 
 import java.awt.Graphics;
 import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.SwingConstants;
+import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Color;
+import java.awt.Font;
 
 public class PlayfieldPanel extends MinoPanel {
     private ObjectDataGrid<MinoColor> renderBlocks = new ObjectDataGrid<>(PANEL_WIDTH_BLOCKS, PANEL_HEIGHT_BLOCKS);
     private PlayerRenderData pdr = null;
     private double playerLockProgress;
+    private CalloutLabel callout;
 
     public void setRenderBlocks(ObjectDataGrid<MinoColor> renderBlocks) {
         this.renderBlocks = renderBlocks;
@@ -21,10 +26,30 @@ public class PlayfieldPanel extends MinoPanel {
     public PlayfieldPanel() {
         super(10, 20);
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        callout = new CalloutLabel();
+        callout.setHorizontalAlignment(SwingConstants.CENTER);
+        callout.setFont(callout.getFont().deriveFont(callout.getFont().getStyle() | Font.BOLD,
+                callout.getFont().getSize() + 6));
+
+        GroupLayout layout = new GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createSequentialGroup()
+                        .addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Integer.MAX_VALUE)
+                        .addComponent(callout)
+                        .addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Integer.MAX_VALUE));
+        layout.setVerticalGroup(
+                layout.createSequentialGroup()
+                        .addGap(PREF_H / 6)
+                        .addComponent(callout));
+
+        callout.startAnimation("WOOOOOWW!!!");
+
     }
 
     @Override
     protected void paintComponent(Graphics g) {
+        super.paintComponents(g);
         for (int y = 0; y < PANEL_HEIGHT_BLOCKS; y++) {
             for (int x = 0; x < PANEL_WIDTH_BLOCKS; x++) {
                 MinoColor currColor = renderBlocks.getAtPos(x, y);
